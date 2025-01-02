c789168f47114b4a1e4394d658424d88276f9035
losanni <58642077+losanni@users.noreply.github.com>
2025-01-01 22:49:49 -0500

    Translation system now live
    - Gets the text from json

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # READ ME
 Julian is short
 
-**Reminder: You can and should use absolute "/" urls. No need for relative ../ or ./** 
+# Reminders: 
+
+You can and should use absolute "/" urls. No need for relative ../ or ./
+
+We manually set the id of elements that we want to translate to avoid listing through irrelevant items
---
+++ locale.js
@@ -0,0 +1,74 @@
+// Translation
+
+// Checks if we have a language set from last time, if not default to user's browser
+let language = localStorage.getItem("lang") || navigator.language
+
+console.log(language) //debug delete later
+
+// Create an event to use later when we change language
+// Events docs: https://developer.mozilla.org/en-US/docs/Web/Events/Creating_and_triggering_events
+const langChange = new CustomEvent("langChange");
+
+// Resposible for page translation, event handler, KEEP THIS RIGHT HERE
+document.addEventListener("langChange", (e) => {
+
+    // If it's french, we apply translations by getting them from json.
+    // The json is a list of IDs of elements which contain text. 
+    // We did it manualy since we don't want to have containers included. When we run "innerText" on a container, it returns text of sub elements.
+    // It then gets the element with the corresponding ids and changes its innerText.
+
+
+    if (navigator.language.includes('fr') || localStorage.getItem("lang") === "fr") {
+        
+        console.log("language changed to fr")
+
+        // Remove ".html" from the end and replace it with "-fr.json" which is where are translations are stored
+        const frTranslationsUrl = window.location.pathname.replace(/\.html$/, "-fr.json")
+
+        // Fetch Docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+        // Gets our translation from the json they're stored in with fetch
+        async function getData() {
+            const url = frTranslationsUrl;
+            try {
+                const response = await fetch(url);
+
+                const frTranslationsAsObj = await response.json();
+                console.log(frTranslationsAsObj)
+
+                // We save it as obj, so we consider it through keys,values
+
+                const ids = Object.keys(frTranslationsAsObj);
+
+                ids.forEach(key => {
+                    let elementToTR = document.getElementById(key);
+
+                    elementToTR.innerText = frTranslationsAsObj[key]
+                });
+
+
+                } catch (error) {
+                    console.error(error.message);
+                }
+        }
+
+        getData()
+    } else {
+        // Otherwise, its english so refresh because our html file is eng
+        console.log("Changed to english") //debug
+        window.location.reload();
+    }
+})
+
+// First check for french
+if (navigator.language.includes('fr') || localStorage.getItem("lang") === "fr") {
+    document.dispatchEvent(langChange)
+}
+
+// Handles the button click
+// Stores the input and calls the handler to translate page
+function setLang(languageUserChose) {
+    console.log(languageUserChose) //debug
+    localStorage.setItem("lang", languageUserChose.toString())
+    
+    document.dispatchEvent(langChange)
+}
